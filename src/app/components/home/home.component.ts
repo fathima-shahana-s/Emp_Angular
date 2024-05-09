0fca1164eba7f5cb302a561384b1ac4b9a917ba6
fathima-shahana-s <fathimashahana231@gmail.com>
2024-05-09 23:15:13 +0530

    export attendance fix

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ExportAttendanceComponent } from '../export-attendance/export-attendance.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ExportAttendanceComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
