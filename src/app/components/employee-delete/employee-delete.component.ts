fb99467eda04e1cb132622aeba647e9b94b41dc2
lakshere <iamlaksme@gmail.com>
2024-05-12 00:03:44 +0530

    Added employee delete component

---
+++ src/app/components/employee-delete/employee-delete.component.ts
@@ -0,0 +1,32 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Employee } from '../../models/employee.model'; 
+
+@Component({
+  selector: 'app-employee-delete',
+  templateUrl: './employee-delete.component.html',
+  styleUrls: ['./employee-delete.component.css']
+})
+export class EmployeeDeleteComponent implements OnInit {
+  employee: Employee;
+
+  constructor(
+    public dialogRef: MatDialogRef<EmployeeDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { employee: Employee }
+  ) {
+    this.employee = {};
+   }
+
+  ngOnInit(): void {
+    this.employee = this.data.employee; // Access employee object from injected data
+  }
+
+  onDelete(): void {
+    console.log('Employee deleted:', this.employee); 
+    this.dialogRef.close(true); 
+  }
+
+  onClose(): void {
+    this.dialogRef.close(false); 
+  }
+}
