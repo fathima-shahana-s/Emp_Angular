cf5153504bd0309e392a3e44c7c0144d7832df62
aaronpeter20 <aaronpeter408@gmail.com>
2024-05-12 23:51:02 +0530

    attendance edit styled

--- src/app/components/add-attendance/add-attendance.component.spec.ts
+++ src/app/components/add-attendance/add-attendance.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FormsModule } from '@angular/forms';
 import { AddAttendanceComponent } from './add-attendance.component';
 
 describe('AddAttendanceComponent', () => {
