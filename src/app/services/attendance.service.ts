08e6378c9b91a7ebe9e4c3cf4d8cbd3510e6cb83
fathima-shahana-s <fathimashahana231@gmail.com>
2024-05-13 22:18:02 +0530

    emp table

--- src/app/services/attendance.service.ts
+++ src/app/services/attendance.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Attendance } from '../models/attendance.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://localhost:8080/api/attendance';
 
 @Injectable({
   providedIn: 'root'
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Employee } from '../models/employee.model';
 
-const baseUrl = 'http://localhost:8080/api/tutorials';
+const baseUrl = 'http://localhost:8080/api/employee';
 
 @Injectable({
   providedIn: 'root'
