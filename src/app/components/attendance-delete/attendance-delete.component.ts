1a92443a7f0f3319f9278fd4501277d3558aa1f6
fathima-shahana-s <fathimashahana231@gmail.com>
2024-05-13 14:08:47 +0530

    dlt attendance class updated

--- src/app/components/attendance-delete/attendance-delete.component.ts
+++ src/app/components/attendance-delete/attendance-delete.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,Inject,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Attendance } from '../../models/attendance.model';
+import { AttendanceService } from '../../services/attendance.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-attendance-delete',
@@ -9,5 +13,53 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './attendance-delete.component.css'
 })
 export class AttendanceDeleteComponent {
+  attendance: Attendance = {attendance_id: 0, employee_id: 0, date: new Date(), status: '' };
 
+  constructor(
+    private attendanceService: AttendanceService,
+    private route: ActivatedRoute,
+    private router: Router) { }
+
+    deleteAttendance(): void {
+      const data={
+      attendance_id:this.attendance.attendance_id,
+      employee_id: this.attendance.employee_id,
+      date: this.attendance.date,
+      status: this.attendance.status,
+      }
+
+      this.attendanceService.delete(this.attendance.attendance_id)
+        .subscribe({
+          next: (res) => {
+            console.log(res);
+            this.router.navigate(['/attendance']);
+          },
+          error: (e) => console.error(e)
+        });
+    }
+  
 }
+
+/*
+attendance:Attendance;
+
+  constructor(
+    public dialogRef: MatDialogRef<AttendanceDeleteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { attendance: Attendance }
+  ) {
+    this.attendance = {};
+   }
+
+  ngOnInit(): void {
+    this.attendance = this.data.attendance; // Access employee object from injected data
+  }
+
+  onDelete(): void {
+    console.log('Attendance deleted:', this.attendance); 
+    this.dialogRef.close(true); 
+  }
+
+  onClose(): void {
+    this.dialogRef.close(false); 
+  }
+  */
