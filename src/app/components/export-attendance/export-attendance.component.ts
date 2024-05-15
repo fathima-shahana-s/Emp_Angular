e9dd471d790e0e9d0fd5ac06953a00ba9a1eb159
JK432 <jk04032002@gmail.com>
2024-05-15 15:04:30 +0530

    b4merge

--- src/app/components/export-attendance/export-attendance.component.ts
+++ src/app/components/export-attendance/export-attendance.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-export-attendance',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './export-attendance.component.html',
   styleUrl: './export-attendance.component.css'
 })
